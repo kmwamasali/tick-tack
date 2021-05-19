2f791cb977ee47aba037e5e62f6aa1386cd00e78
kmwamasali <kmwamasali@gmail.com>
2021-05-19 13:16:42 +0300

    Feature: Setup Tick tack game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i) {
+    const { xIsNext, squares } = this.state
+    const squared = squares.slice()
+    squared[i] = xIsNext ? 'X' : 'O';
+    return this.setState({ 
+      squares: squared,
+      xIsNext: !xIsNext,
+     })
+  }
+
+  renderSquare(i) {
+    return <Square 
+             value={this.state.squares[i]}
+             onClick={() => this.handleClick(i)} />;
+  }
+
+  render() {
+    const status = `Next Player ${this.state.xIsNext ? 'X' : 'O'}`;
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* TODO */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
